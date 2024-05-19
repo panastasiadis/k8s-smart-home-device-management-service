82f99d349872c4fa4533c0efacda72fcceeae86f
paanastasiadis <panag1234@outlook.com>
2024-05-19 16:56:50 +0300

    Add actions to admin page

--- device_management/devices/admin.py
+++ device_management/devices/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import Device, Room, Sensor
+from .models import Action, Device, Room, Sensor
 
 # Register your models here.
 admin.site.register(Device)
 admin.site.register(Sensor)
 admin.site.register(Room)
+admin.site.register(Action)
